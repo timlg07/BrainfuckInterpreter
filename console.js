0f49ca5bc101af2acd704f280ad43aa996c9968d
T!M Greller <timlg07@gmail.com>
2019-11-16 22:05:21 +0100

    implemented console.js

--- console.js
+++ console.js
@@ -1,4 +1,16 @@
 class Console
 {
+    constructor( html_element )
+    {
+        this.element = html_element;
+    }
     
+    log( text, color='#fff' ){
+        this.element.innerHTML+=`<span style="color:${color}">${text}</span><br>`;
+        this.element.scrollTop = this.element.scrollHeight;
+    }
+    
+    error( text ){
+        this.log( text,'red' );
+    }
 }
