a7cb3721f00b19cc884bbcda8af5d20ec2a3f15f
T!M Greller <timlg07@gmail.com>
2019-11-18 16:06:24 +0100

    added getter and setter for the pointer

--- interpreter.js
+++ interpreter.js
@@ -12,4 +12,27 @@ class FuckingInterpreter
         this.console = new Console( console );
     }
     
+    get pointer(){
+        return this._pointer.value;
+    }
+    set pointer( n ){
+        if( n < 0 ){
+            this.console.error("Pointer cannot be negative.");
+            return;
+        } else if( n >= this.storage.amount ){
+            this.storage.addStorageCells( n - (this.storage.amount-1) );
+            this.console.log("Storage size increased.");
+        }
+        
+        let set_ptr =(_="")=> this.storage.element.querySelector("tr:nth-child("+(this._pointer.value+2)+") td:first-child").innerHTML=_;
+        
+        set_ptr();
+        this._pointer.value = n;
+        set_ptr(">");
+        
+        this._pointer.element.innerText = this._pointer.value;
+    }
+    
+    
+    
 }
